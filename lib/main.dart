2ed8040c089337f2ed1edb83fa27d0ad3d511e82
Aswin Asok <aswinasokofficial@gmail.com>
2025-02-03 12:09:39 +0530

    feat(qrApp):started

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:qrapp/pages/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'Satoshi'),
+      title: 'QR App',
+      home: Home(),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
